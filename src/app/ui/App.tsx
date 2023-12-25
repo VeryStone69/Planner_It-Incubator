2981cf4dba6b43c7c84ad9b6b793ed95d7489adb
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-12-25 23:14:40 +0100

    add: NotFoundPage component

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -39,6 +39,7 @@ function App() {
                     <Routing/>
                 </Container>
             </div>
+
         </ThemeProvider>
     )
 }
--- src/app/ui/Routing/Routing.tsx
+++ src/app/ui/Routing/Routing.tsx
@@ -2,6 +2,7 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import {TodolistsList} from "../../../features/TodolistsList/ui/TodolistsList";
 import {Login} from "../../../features/auth/ui/Login";
 import React from "react";
+import {NotFoundPage} from "../NotFoundPage/NotFoundPage";
 
 export const Routing = () => {
     const ROUTES = {
@@ -12,16 +13,10 @@ export const Routing = () => {
     };
     return (
         <Routes>
-            {/*Delete later*/}
-                {/*<Route path={ROUTES.NOT_FOUND} element={<h1>404: Page NOT FOUND</h1>}/>*/}
-                {/*<Route path="/*" element={<Navigate to="/404"/>}/>*/}
-                {/*<Route path={ROUTES.TODO_LIST} element={<TodolistsList/>}/>*/}
-                {/*<Route path={ROUTES.HOME} element={<TodolistsList/>}/>*/}
-                {/*<Route path={ROUTES.LOGIN} element={<Login/>}/>*/}
             <Route path={ROUTES.HOME} element={<Navigate to={ROUTES.TODO_LIST} replace />} />
             <Route path={ROUTES.LOGIN} element={<Login />} />
             <Route path={ROUTES.TODO_LIST} element={<TodolistsList />} />
-            <Route path={ROUTES.NOT_FOUND} element={<Navigate to="/404"/>}/>
+            <Route path={ROUTES.NOT_FOUND} element={<NotFoundPage/>}/>
             <Route path="/*" element={<Navigate to={ROUTES.NOT_FOUND} />} />
         </Routes>
     )
