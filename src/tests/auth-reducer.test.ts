9b3706be714771d4095133863bcf4851478bf0b6
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2023-12-26 17:42:57 +0100

    add: unit tests for auth-reducer

---
+++ src/tests/auth-reducer.test.ts
@@ -0,0 +1,55 @@
+import {authReducer, authThunks} from "../features/auth/model/auth-reducer";
+
+describe('auth reducer', () => {
+    let initialState = {
+        isLoggedIn: false,
+    };
+
+    beforeEach(() => {
+        initialState = {
+            isLoggedIn: false,
+        };
+    });
+
+    test('should handle initial state', () => {
+        expect(authReducer(undefined, {type: 'unknown'})).toEqual({
+            isLoggedIn: false,
+        });
+    });
+
+    test('should handle login', () => {
+        const action = authThunks.login
+            .fulfilled({isLoggedIn: true}, 'requestId', {
+            email: 'free@samuraijs.com',
+            password: 'free',
+            rememberMe: true
+        });
+        const endState = authReducer(initialState, action);
+        expect(endState).toEqual({isLoggedIn: true});
+    });
+
+    test('should handle logout', () => {
+        const action = authThunks.logout
+            .fulfilled({isLoggedIn: false}, 'requestId', undefined);
+        const endState = authReducer({isLoggedIn: true}, action);
+        expect(endState).toEqual({isLoggedIn: false});
+    });
+
+    test('should handle login rejected', () => {
+        const action = authThunks.login.rejected(null, 'requestId', {
+            email: 'free@samuraijs.com',
+            password: 'wrong',
+            rememberMe: false
+        });
+        const endState = authReducer(initialState, action);
+        expect(endState).toEqual({isLoggedIn: false});
+    });
+
+    test('should handle app initialization when not logged in', () => {
+        const action = authThunks.initializeApp
+            .fulfilled({isLoggedIn: false}, 'requestId', undefined);
+        const endState = authReducer(initialState, action);
+        expect(endState).toEqual({isLoggedIn: false});
+    });
+});
+
