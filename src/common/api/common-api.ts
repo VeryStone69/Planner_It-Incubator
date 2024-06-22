5500d53c467fa943b4c2c88bfe5e9a8e9a296179
Aliaksandr Kaptsevich <carcer2007@gmail.com>
2024-06-22 19:39:30 +0200

    update: api authorization 2

--- src/common/api/common-api.ts
+++ src/common/api/common-api.ts
@@ -5,7 +5,8 @@ export const instance = axios.create({
     withCredentials: true,
     headers: {
         'API-KEY': '05ded78c-1af5-47a8-9e58-4fefeb749215',
-        Authorization: 'Bearer 8c9566a0-c81e-47f7-9f0c-8d0024d5b916'
+        // Authorization: 'Bearer 8c9566a0-c81e-47f7-9f0c-8d0024d5b916' old version
+        Authorization: 'Bearer 5a3771d8-afb1-436b-864c-5c47553d9820'
     }
 })
 
